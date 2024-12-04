82ea62e7307897b56ae70fcff5f9e4f4d91802fc
Dan Ruan <danrsaquetti@gmail.com>
2024-12-03 23:12:25 -0300

    feat: Criando teste de Criação de Tarefa no Klist

--- Danrley/tests.ts
+++ Danrley/tests.ts
@@ -7,7 +7,10 @@ const TIME_DELAY_NEXT_STEP = TIME_MILLISECONDS * 2
 const TIMEOUT_ELEMENT_LOCATED = TIME_MILLISECONDS * 3
 
 async function testRegisterListTask() {
-    let driver = await new Builder().forBrowser('chrome').build()
+    let driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(new (require('selenium-webdriver/chrome').Options)().addArguments('--start-fullscreen'))
+        .build()
 
     try {
         await driver.get(BASE_URL)
@@ -50,7 +53,10 @@ async function testRegisterListTask() {
 }
 
 async function testRegisterTask() {
-    let driver = await new Builder().forBrowser('chrome').build()
+    let driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(new (require('selenium-webdriver/chrome').Options)().addArguments('--start-fullscreen'))
+        .build()
 
     try {
         await driver.get(BASE_URL)
@@ -77,6 +83,81 @@ async function testRegisterTask() {
         )
 
         await loginField.sendKeys('Fazer o trabalho de Plano de Testes', Key.RETURN)
+
+        await driver.sleep(TIME_MILLISECONDS * 10)
+
+    } finally {
+        await driver.close()
+    }
+}
+
+async function testConfigTask() {
+    let driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(new (require('selenium-webdriver/chrome').Options)().addArguments('--start-fullscreen'))
+        .build()
+
+    try {
+        await driver.get(BASE_URL)
+        await driver.sleep(TIME_DELAY_NEXT_STEP)
+
+        await efetuarLogin(driver)
+
+        await driver.sleep(TIME_DELAY_NEXT_STEP)
+
+        const list = await driver.wait(
+            until.elementLocated(By.xpath('//li[@role="listitem" and .//i[@data-icon-name="Edit"]][1]')),
+            TIMEOUT_ELEMENT_LOCATED
+        )
+
+        await driver.sleep(TIME_DELAY_NEXT_STEP)
+
+        await list.click()
+
+        await driver.sleep(TIME_DELAY_NEXT_STEP)
+
+        const task = await driver.wait(
+            until.elementLocated(By.xpath('//div[@role="listitem" and @data-list-index="0"]')),
+            TIMEOUT_ELEMENT_LOCATED
+        )
+
+        await task.click()
+
+        await driver.sleep(TIME_DELAY_NEXT_STEP)
+
+        const dateInput = await driver.wait(
+            until.elementLocated(By.xpath('//input[@placeholder="Definir data"]')),
+            TIMEOUT_ELEMENT_LOCATED
+        )
+
+        await dateInput.click()
+
+        await driver.sleep(TIME_DELAY_NEXT_STEP)
+
+        const dateDayInput = await driver.wait(
+            until.elementLocated(By.xpath('//table[contains(@class, "ms-DatePicker-table")]/tbody/td[1]/button')),
+            TIMEOUT_ELEMENT_LOCATED
+        )
+
+        await dateDayInput.click()
+
+        await driver.sleep(TIME_DELAY_NEXT_STEP)
+
+        const subtaskField = await driver.wait(
+            until.elementLocated(By.xpath(`//input[@name='subtask']`)),
+            TIMEOUT_ELEMENT_LOCATED
+        )
+
+        await subtaskField.sendKeys(process.env.AUTH_EMAIL || '', Key.RETURN)
+
+        await driver.sleep(TIME_DELAY_NEXT_STEP)
+
+        const addTaskButton = await driver.wait(
+            until.elementLocated(By.xpath('//div[@id="subtasks"]/i[@data-icon-name="Add"]')),
+            TIMEOUT_ELEMENT_LOCATED
+        )
+
+        await addTaskButton.click()
 
         await driver.sleep(TIME_MILLISECONDS * 10)
 
@@ -124,4 +205,5 @@ async function efetuarLogin(driver: WebDriver) {
 }
 
 // testRegisterListTask()
-testRegisterTask()
+// testRegisterTask()
+testConfigTask()
